0cde2a766f8a51868e92186c001b33bd9dd8db22
Gabriel Oliveira <gab.oliveira@outlook.com>
2019-07-31 09:30:19 -0300

    Simplify TravisCI script

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 docker build -t fireworkweb/node:alpine alpine/
 docker build -t fireworkweb/node:qa qa/
-docker build -t fireworkweb/node:8-alpine 8-alpine/
 docker build -t fireworkweb/node:10-alpine 10-alpine/
+docker build -t fireworkweb/node:8-alpine 8-alpine/
