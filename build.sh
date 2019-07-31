3079e71a96ee08fae403cfd3857a24725732196d
Gabriel Oliveira <gab.oliveira@outlook.com>
2019-07-31 16:32:10 -0300

    Fix su-exec developer on entrypoints

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-docker build -t fireworkweb/node:alpine alpine/
-docker build -t fireworkweb/node:qa qa/
-docker build -t fireworkweb/node:10-alpine 10-alpine/
-docker build -t fireworkweb/node:8-alpine 8-alpine/
+docker build -t fireworkweb/node:alpine alpine
+docker build -t fireworkweb/node:qa qa
+docker build -t fireworkweb/node:10-alpine 10-alpine
+docker build -t fireworkweb/node:8-alpine 8-alpine
