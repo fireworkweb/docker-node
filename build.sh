60010a04ee710f1917ff7b568719353b88e5e88b
Gabriel Oliveira <gab.oliveira@outlook.com>
2020-02-18 15:33:39 -0300

    Add 12-alpine version

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 
 docker build -t fireworkweb/node:alpine alpine
 docker build -t fireworkweb/node:qa qa
+docker build -t fireworkweb/node:12-alpine 12-alpine
 docker build -t fireworkweb/node:10-alpine 10-alpine
 docker build -t fireworkweb/node:8-alpine 8-alpine
