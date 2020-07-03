60ea2e6136361b0eb72712e91112d6fce868a887
Daniel Polito <danielbpolito@gmail.com>
2020-07-03 13:41:50 -0300

    GH Action

--- build.sh
+++ build.sh
@@ -1,12 +1,18 @@
 #! /bin/bash
 
 docker build --pull -t fireworkweb/node:8 8
-docker build --pull -t fireworkweb/node:8-qa 8-qa
+docker build -t fireworkweb/node:8-adonis 8-adonis
+docker build -t fireworkweb/node:8-adonis-qa 8-adonis-qa
+docker build -t fireworkweb/node:8-qa 8-qa
 docker build --pull -t fireworkweb/node:10 10
-docker build --pull -t fireworkweb/node:10-qa 10-qa
+docker build -t fireworkweb/node:10-adonis 10-adonis
+docker build -t fireworkweb/node:10-adonis-qa 10-adonis-qa
+docker build -t fireworkweb/node:10-qa 10-qa
 docker build --pull -t fireworkweb/node:12 12
-docker build --pull -t fireworkweb/node:12-qa 12-qa
+docker build -t fireworkweb/node:12-adonis 12-adonis
+docker build -t fireworkweb/node:12-adonis-qa 12-adonis-qa
+docker build -t fireworkweb/node:12-qa 12-qa
 docker build --pull -t fireworkweb/node:14 14
-docker build --pull -t fireworkweb/node:14-qa 14-qa
-docker build --pull -t fireworkweb/node:latest latest
-docker build --pull -t fireworkweb/node:qa qa
+docker build -t fireworkweb/node:14-adonis 14-adonis
+docker build -t fireworkweb/node:14-adonis-qa 14-adonis-qa
+docker build -t fireworkweb/node:14-qa 14-qa
